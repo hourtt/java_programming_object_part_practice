82b30ebeb56d5ef7d4eaf77f231c51d52e2883b4
hourtt <bunhourtjesda@gmail.com>
2025-07-25 20:43:31 +0700

    This second student file is using final for declare the name and age values in the constructor(Recommend)

--- src/main/java/com/example/object_part_practice/student/Student_2.java
+++ src/main/java/com/example/object_part_practice/student/Student_2.java
@@ -1,4 +1,34 @@
 package com.example.object_part_practice.student;
 
-public class student_2 {
+import java.util.Objects;
+
+public class Student_2 {
+    private final String name;
+    private final int age;
+
+    public Student_2(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null || getClass() != object.getClass()) return false;
+        Student_2 student2 = (Student_2) object;
+        return age == student2.age && Objects.equals(name, student2.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Student_2{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
 }
